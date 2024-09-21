89dfec5e939555c2ba628c76bba175c3fc433c33
Znaxh <anuragpratapsingh.4845@gmail.com>
2024-09-22 00:46:57 +0530

    changes made to the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,390 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100 text-gray-700 py-10">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {/* Integrations */}
+          <div>
+            <h3 className="text-lg font-bold mb-4">Integrations</h3>
+            <ul>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Shopify
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Etsy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  eBay
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Amazon
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Tik Tok Shop
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  PrestaShop
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  BigCommerce
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Wix
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  WooCommerce
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Squarespace
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Pritify API
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Pritify Pop-Up-Store
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Shutterstock
+                </a>
+              </li>
+            </ul>
+          </div>          
+
+          {/* Discover */}
+          <div>
+            <h3 className="text-lg font-bold mb-4">Discover</h3>
+            <ul>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Guides
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Etsy print-on-demand
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Shopify print-on-demand
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Woocommerce print-on-demand
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Wix print-on-demand
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Squarespace print-on-demand
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Make Your Own Shirt
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Brands
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Shipping Rates
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Mockup Generator
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Start selling */}
+          <div>
+            <h3 className="text-lg font-bold mb-4">Start selling</h3>
+            <ul>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom T-shirts
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Hoodies
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Mugs
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Socks
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Backpacks
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Branding
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Sell on Etsy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Sell on Social Media
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Free T-shirt Designs
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Products
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom Products
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Custom All-Over-Print Hoodies
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Start a Clothing Line 
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Start POD Bussiness
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Bulk Orders
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Transfering To Printify
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Printify */}
+          <div>
+            <h3 className="text-lg font-bold mb-4">Printify</h3>
+            <ul>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Print on Demand 
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Print Providers
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Experts Program
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Printify Express Delivery
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  Become a Partner
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Printify Quality Promise
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Jobs
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Webinars
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Printing Profits Podcast
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Contact Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Affiliate
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Contact Sales
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                POD Glossary
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Network Fulfillment Status
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Merchant Protection
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Security
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-green-600 transition">
+                Sitemap
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <hr className="my-8 border-gray-300" />
+
+        <div className="flex flex-col text-gray-500 sm:flex-row justify-center  items-center text-sm">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+              Intellectual Property Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+              Terms of Service
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+              Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+              Security
+              </a>
+            </li>
+          </ul>
+        </div>
+        
+        <div className="flex flex-col sm:flex-row justify-between items-center text-sm">
+          <p>&copy; {new Date().getFullYear()} Printify, Inc. All rights reserved.</p>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+                Terms of Service
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-green-600 transition">
+                Cookie Policy
+              </a>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
