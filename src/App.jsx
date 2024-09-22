00f109ed3548a98f51695b94d991d59828b43c06
Znaxh <anuragpratapsingh.4845@gmail.com>
2024-09-22 15:11:44 +0530

    need to make the changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Socials from './components/Socials'
+import Testimonial from './components/Testimonial'
+
 const App = () => {
   return (
     <div>
       {/* <Navbar /> */}
       {/* <Footer /> */}
-      <Socials />
+      {/* <Socials /> */}
+      <Testimonial />
     </div>
   )
 }
